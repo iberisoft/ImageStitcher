992315f18e472a1ec80aab49fd175071e4020484
Pavel Zaytsev <pavel@iberisoft.com>
2022-10-23 12:05:40 +0200

    Add project files.

---
+++ ImageStitcher/Program.cs
@@ -0,0 +1,33 @@
+﻿using Emgu.CV;
+using Emgu.CV.Stitching;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ImageStitcher
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePaths = Directory.GetFiles(args[0], "*.tif");
+            var images = filePaths.Select(filePath => new Image<Gray, byte>(filePath)).ToList();
+            using var resultImage = new Image<Gray, byte>(1000, 1000);
+
+            StitchImages(images, resultImage);
+
+            foreach (var image in images)
+            {
+                image.Dispose();
+            }
+        }
+
+        private static void StitchImages(List<Image<Gray, byte>> images, Image<Gray, byte> resultImage)
+        {
+            using var stitcher = new Stitcher(Stitcher.Mode.Scans);
+            stitcher.Stitch(new VectorOfMat(images.Select(image => image.Mat).ToArray()), resultImage.Mat);
+        }
+    }
+}
